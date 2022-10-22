c329078535b861de1edd1bb0658948851ba86e65
212021 <212021@1220-013-22>
2022-10-22 14:04:22 +0300

    Добавьте файлы проекта.

---
+++ BlazorContolWork/Data/User.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlazorContolWork.Data
+{
+    public class User
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public User(string name, string surName, string email, string password, string login)
+        {
+            Name = name;
+            Surname = surName;
+            Password = password;
+            Email = email;
+            Login = login;
+        }
+    }
+}
