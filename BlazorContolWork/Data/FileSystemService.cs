c7963fbc24a4864769fb59f3db16dcfa871ab543
Арсений Фролов <tincofa@yandex.ru>
2022-10-30 22:47:38 +0300

    refFilesAddNewColectionSystem32Bit+4G

--- BlazorContolWork/Data/FileSystemService.cs
+++ BlazorContolWork/Data/FileSystemService.cs
@@ -1,78 +1,21 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-using MongoDB.Bson;
+﻿using MongoDB.Driver.GridFS;
 using MongoDB.Driver;
-using MongoDB.Driver.GridFS;
-using System;
-using System.Diagnostics;
 
 namespace BlazorContolWork.Data
 {
     static public class FileSystemService
     {
-        static public void Initialization()
+        static public async Task UploadFileToDb(string path, string fileName)
         {
             var client = new MongoClient();
-            var database = client.GetDatabase("ImageDB");
-            var gridFS = new GridFSBucket(database);
-        }
-
-        static public async Task UploadImageToDb(string path, string fileName, User user)
-        {
-            var client = new MongoClient();
-            var database = client.GetDatabase("ImageDB");
+            var database = client.GetDatabase("FileDB");
             var gridFS = new GridFSBucket(database);
 
             using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
             {
                 gridFS.UploadFromStream(fileName, fs);
             }
-            //File.Delete(path);
-            await SearchByNameReplaceUser(fileName, user);
-        }
-
-        private static async Task SearchByNameReplaceUser(string fileName, User user)
-        {
-            var client = new MongoClient();
-            var database = client.GetDatabase("ImageDB");
-            var gridFS = new GridFSBucket(database);
-
-            var filter = Builders<GridFSFileInfo>.Filter.Eq<string>(info => info.Filename, fileName);
-            var fileInfos = (await gridFS.FindAsync(filter)).FirstOrDefault();
-            user._idPhoto = fileInfos.Id;
-            MongoExamples.ReplaceById(user._id, user);
-        }
-        
-        public static string GetNameInDb(User user)
-        {
-            var client = new MongoClient();
-            var database = client.GetDatabase("ImageDB");
-            var gridFS = new GridFSBucket(database);
-
-            var filter = Builders<GridFSFileInfo>.Filter.Eq("_id", user._idPhoto);
-            string fileNames = string.Empty;
-
-            using (var cursor = gridFS.Find(filter))
-            {
-                fileNames = cursor.FirstOrDefault().Filename;
-            }
-            return fileNames;
-        }
-
-        static public void DownloadToLocal(string webRootPath, User user)
-        {
-            var client = new MongoClient();
-            var database = client.GetDatabase("ImageDB");
-            var gridFS = new GridFSBucket(database);
-
-            using (Stream fs = new FileStream($"{webRootPath}\\Images\\{GetNameInDb(user)}", FileMode.Create))
-            {
-                try
-                {
-                    gridFS.DownloadToStream(user._idPhoto, fs);
-                }
-                catch { }
-            }
-            
+            File.Delete(path);
         }
     }
 }
